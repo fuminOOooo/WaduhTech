6a9469aa3dd81337a05db6f382b61bfd27d06b5f
Areez <31994090+azer0ne@users.noreply.github.com>
2023-06-25 19:27:14 +0700

    Add: Main Menu
    - adding MainMenu scene
    - change didLoad to MainMenu scene
    - change GameOver scene

--- WaduhTechProj/WaduhTechProj Shared/Game Over/GameOver.swift
+++ WaduhTechProj/WaduhTechProj Shared/Game Over/GameOver.swift
@@ -11,19 +11,32 @@ import GameplayKit
 
 class GameOver: SKScene {
     
+    var mainMenuButton: SKSpriteNode!
+    var startLocation: CGPoint? = nil
+
     public var win = true
     
     override func didMove(to view: SKView) {
         // Set up the scene
         let label = self.childNode(withName: "//gameOverLabel") as? SKLabelNode
         if win == false {
-            label?.text = "Try again"
+            label?.text = "You Failed!"
+            let jumpscare = SKSpriteNode(imageNamed: "jumpscare")
+            jumpscare.position = CGPoint(x: 0, y: 200)
+            jumpscare.zPosition = 2
+            jumpscare.size = CGSize(width: 600, height: 600)
+            addChild(jumpscare)
         }
-        
+        mainMenuButton = SKSpriteNode(imageNamed: "mainMenuButton")
+//        mainMenuButton.zPosition = 2
+        mainMenuButton.position = CGPoint(x: 0, y: -200)
+        mainMenuButton.size = CGSize(width: 500, height: 100)
+        addChild(mainMenuButton)
+        print("button loaded")
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        let scene = GameScene(fileNamed: "GameScene")
+        let scene = MainMenu(fileNamed: "MainMenu")
         scene!.scaleMode = .aspectFit
         self.view?.presentScene(scene)
     }
